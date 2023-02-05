6dcdd3a8ec21c1f8d62e1a41ecc4e8854bf3f293
Andrei ~Tache <takeandrei5@gmail.com>
2023-02-05 12:29:11 +0200

    fix: added back trpc page ts

---
+++ src/pages/api/trpc/[trpc].page.ts
@@ -0,0 +1,19 @@
+import { createNextApiHandler } from "@trpc/server/adapters/next";
+
+import { env } from "../../../env/server.mjs";
+import { createContext } from "../../../server/trpc/context.js";
+import { appRouter } from "../../../server/trpc/router/_app.js";
+
+// export API handler
+export default createNextApiHandler({
+  router: appRouter,
+  createContext,
+  onError:
+    env.NODE_ENV === "development"
+      ? ({ path, error }) => {
+          console.error(
+            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`
+          );
+        }
+      : undefined,
+});
