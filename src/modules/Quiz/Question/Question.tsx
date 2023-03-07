13619e67fe7504da9037df48bc98a0f822fae38f
Andrei ~Tache <takeandrei5@gmail.com>
2023-03-07 23:34:43 +0200

    chore: removed blur

--- src/modules/Quiz/Question/Question.tsx
+++ src/modules/Quiz/Question/Question.tsx
@@ -1,25 +1,17 @@
 import Image from "next/image";
 
 import type { QuestionProps } from "./types";
-import { useState } from "react";
-import cx from "classnames";
 
 const Question: React.FC<QuestionProps> = ({
   image = "",
   question = "",
 }: QuestionProps) => {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
   return (
     <>
       {image && (
-        <figure
-          className={cx(
-            "drag-none pointer-events-none relative h-24 w-24 select-none "
-          )}
-        >
+        <figure className="drag-none pointer-events-none relative h-24 w-24 select-none ">
           <Image
-            className={cx("max-h-full duration-700 ease-in-out", {})}
+            className="max-h-full"
             src={image}
             alt={question}
             width={96}
@@ -29,7 +21,6 @@ const Question: React.FC<QuestionProps> = ({
             style={{
               objectFit: "contain",
             }}
-            onLoadingComplete={() => setIsLoading(false)}
           />
         </figure>
       )}
