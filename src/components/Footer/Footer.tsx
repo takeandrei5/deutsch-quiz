1971b262a06fcc3399008c0a87212e59a9525316
Andrei ~Tache <takeandrei5@gmail.com>
2023-03-17 07:20:57 +0200

    chore: removed company name

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,7 @@
 const Footer: React.FC = () => {
   return (
     <footer className="footer footer-center mt-auto pt-4 text-base-content">
-      <span>
-        {`© ${new Date().getFullYear()} Quintix Technologies Ltd. All rights reserved.`}
-      </span>
+      <span>{`© ${new Date().getFullYear()} All rights reserved.`}</span>
     </footer>
   );
 };
