f78322cf45ba41396c9b2bab501226c9469bde9f
Andrei Tache <takeandrei5@gmail.com>
2023-04-18 16:16:00 +0300

    fix: fixed quality issue that caused preloading to fail

--- src/modules/Quiz/QuizGame/hooks/usePreloadImage.tsx
+++ src/modules/Quiz/QuizGame/hooks/usePreloadImage.tsx
@@ -4,27 +4,27 @@ import React from 'react';
 import type { MultipleQuizQuestion } from '@prisma/client';
 
 const usePreloadImage = () => {
-  const preloadImage = (history: JSX.Element[], remainingQuestions: MultipleQuizQuestion[]): JSX.Element[] => {
-    const currentQuestion: MultipleQuizQuestion = remainingQuestions[0];
-    const newHistory: JSX.Element[] = [...history];
+	const preloadImage = (history: JSX.Element[], remainingQuestions: MultipleQuizQuestion[]): JSX.Element[] => {
+		const currentQuestion: MultipleQuizQuestion = remainingQuestions[0];
+		const newHistory: JSX.Element[] = [...history];
 
-    if (currentQuestion && currentQuestion.image) {
-      newHistory.push(
-        React.createElement(PreloadedImage, {
-          key: `${currentQuestion.image}-preload-${currentQuestion.id}`,
-          alt: currentQuestion.correctAnswer,
-          src: currentQuestion.image,
-          width: 96,
-          height: 96,
-          quality: 50,
-        })
-      );
-    }
+		if (currentQuestion && currentQuestion.image) {
+			newHistory.push(
+				React.createElement(PreloadedImage, {
+					key: `${currentQuestion.image}-preload-${currentQuestion.id}`,
+					alt: currentQuestion.correctAnswer,
+					src: currentQuestion.image,
+					width: 96,
+					height: 96,
+					quality: 75
+				})
+			);
+		}
 
-    return newHistory;
-  };
+		return newHistory;
+	};
 
-  return { preloadImage };
+	return { preloadImage };
 };
 
 export { usePreloadImage };
